d869fd0e2dc5e200c6fb3b1f7cc6998b5bc746d3
Adi Chirilov <developer@rosoftware.ro>
2019-09-24 12:48:48 +0300

    Initial Import

---
+++ app/data.py
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+"""
+Fully Coded App by AppSeed.us
+License: commercial
+Read more at https://appseed.us/pricing
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+# Resources used 
+class Data:
+
+    copy       = 'AppSeed'
+
+    email      =  'support@appseed.us'
+    design     =  'Multiverse by Html5Up'
+    
+    seo_title  = 'Flask Multiverse - Built with Automation Tools | AppSeed'
+    seo_desc   = 'Flask app with Multiverse design crafted by Html5Up agency.'
+    seo_keys   = 'flask, multiverse, Html5 Up, appseed'
+
+    product    = 'https://appseed.us/apps/flask-apps/flask-html5up-multiverse'
+    github     = 'https://github.com/app-generator/flask-html5up-multiverse'
+    docs       = 'https://docs.appseed.us/apps/flask-apps/flask-html5up-multiverse/'
+    appseed    = 'https://appseed.us'      
+    generator  = 'https://appseed.us/app-generator'
+    blog       = 'https://blog.appseed.us/'
+    facebook   = 'https://facebook.com/webappseed'
+    twitter    = 'https://twitter.com/webappseed'
+    instagram  = 'https://instagram.com/webappseed'
+    support    = 'https://appseed.us/support'
+    discord    = 'https://discord.gg/fZC6hup'
